3e4eef87d39042a64cc3d016d5faea59f5862823
NachmanBerkowitz <bernach4@gmail.com>
2018-06-18 18:47:47 -0400

    fun wit functions

---
+++ 54/hw54methodCb.js
@@ -0,0 +1,43 @@
+function mySome(array,methodIsAny){
+    for(let i = 0; i < array.length; i++){
+        if(methodIsAny(array[i])){
+            return true;
+        }
+    }
+    return false;
+}
+
+const isUppercase = (char)=>{
+    return char >= 'A' && char <='Z';
+};
+
+const arrayWithUpper = ['A','b','c'];
+const arrayWithoutUpper = ['a','b','c'];
+
+const a = 'with uppercase '+ mySome(arrayWithUpper,(isUppercase)) ;
+const b = 'without uppercase '+ mySome(arrayWithoutUpper,(isUppercase)) ;
+
+const c = 'built in \'some\' with uppercase ' + arrayWithUpper.some(isUppercase);
+const d = 'built in \'some\' without uppercase ' + arrayWithoutUpper.some(isUppercase);
+
+console.log(a+'\n', b+'\n', c+'\n', d+'\n');
+
+//////////////////
+
+function onlyIf(array, test, action){
+    for( let i = 0; i < array.length; i++){
+        if(test(array[i])){
+            action(array[i]);
+        }
+    }
+}
+
+function onlyIfTwo(array, test, action){
+    array.filter(test).forEach(action);
+}
+
+onlyIf(arrayWithUpper,isUppercase, (i)=>console.log('onlyIf ',i));
+onlyIfTwo(arrayWithUpper,isUppercase, (i)=>console.log('onlyIf ',i));
+
+
+ 
