65ca015378b36725248cd7714932ad1974648d71
NachmanBerkowitz <bernach4@gmail.com>
2018-06-19 16:27:57 -0400

    fixed up homework

--- 54/hw54methodCb.js
+++ 54/hw54methodCb.js
@@ -11,16 +11,24 @@ const isUppercase = (char)=>{
     return char >= 'A' && char <='Z';
 };
 
-const arrayWithUpper = ['A','b','c'];
-const arrayWithoutUpper = ['a','b','c'];
+const isLowercase = (char)=>{
+    return char >= 'a' && char <='z';
+};
 
-const a = 'with uppercase '+ mySome(arrayWithUpper,(isUppercase)) ;
-const b = 'without uppercase '+ mySome(arrayWithoutUpper,(isUppercase)) ;
+const arrayMixCase = ['A','b','c'];
+const arrayAllLower = ['a','b','c'];
+const arrayAllUpper = ['A','B','C'];
 
-const c = 'built in \'some\' with uppercase ' + arrayWithUpper.some(isUppercase);
-const d = 'built in \'some\' without uppercase ' + arrayWithoutUpper.some(isUppercase);
 
-console.log(a+'\n', b+'\n', c+'\n', d+'\n');
+const a = 'is upper with uppercase '+ mySome(arrayMixCase,(isUppercase)) ;
+const b = 'is lower with lowercase '+ mySome(arrayMixCase,(isLowercase)) ;
+const c = 'is upper without uppercase '+ mySome(arrayAllLower,(isUppercase)) ;
+const d = 'is lower without lowercase '+ mySome(arrayAllUpper,(isLowercase)) ;
+
+const e = 'built in \'some\' with uppercase ' + arrayMixCase.some(isUppercase);
+const f = 'built in \'some\' without uppercase ' + arrayAllLower.some(isUppercase);
+
+console.log(a+'\n', b+'\n', c+'\n', d+'\n', e+'\n', f+'\n');
 
 //////////////////
 
