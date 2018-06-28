7ebbce69d26a40ef72ddf05a898b3617f2518d12
NachmanBerkowitz <bernach4@gmail.com>
2018-06-27 20:31:31 -0400

    spelling correction

--- 55/functions.js
+++ 55/functions.js
@@ -19,6 +19,7 @@ console.log(Dow.getNumOfDay('Sunday'));
  */
 const Interest = (function () {
     'use strict';
+
     /**
      *The amount of years that interest is accruing.
      *  @type integer;
@@ -29,6 +30,10 @@ const Interest = (function () {
     let compoundingInterest = false;
     let includePrincipalInOutput = true;
 
+    /**
+     * Tests if input is null or undefined
+     * @param {any} input input to test
+     */
     const testNoNullOrUndef = (input) => {
         return (input !== null) && (input !== undefined);
     };
@@ -66,10 +71,10 @@ const Interest = (function () {
             }
             return interstCalcObj;
         },
-        setIncludePrincable: (includePrincable) => {
-            if (testNoNullOrUndef(includePrincable)
-                && testIfBoolean(includePrincable)) {
-                includePrincipalInOutput = includePrincable;
+        setIncludePrincipal: (includePrincipal) => {
+            if (testNoNullOrUndef(includePrincipal)
+                && testIfBoolean(includePrincipal)) {
+                includePrincipalInOutput = includePrincipal;
             }
             return interstCalcObj;
         },
@@ -83,7 +88,7 @@ const Interest = (function () {
                     - (includePrincipalInOutput ? 0 : moneyIn);
         },
         interest: (money, amountOfYears, amountOfInterest, isCompounding,includePrincable) => {
-            return interstCalcObj.setYears(amountOfYears).setInterest(amountOfInterest).setIsCompounding(isCompounding).setIncludePrincable(includePrincable).howMuchInterest(money);
+            return interstCalcObj.setYears(amountOfYears).setInterest(amountOfInterest).setIsCompounding(isCompounding).setIncludePrincipal(includePrincable).howMuchInterest(money);
         }
     };
     return interstCalcObj;
@@ -96,7 +101,7 @@ amount = Interest.setYears(2).howMuchInterest();
 console.log('b',amount);
 
 amount = Interest.setIsCompounding(true)
-    .setIncludePrincable(false)
+    .setIncludePrincipal(false)
     .howMuchInterest();
 console.log('c',amount);
 
@@ -104,7 +109,7 @@ Interest.setInterest(.20);
 amount = Interest.howMuchInterest();
 console.log('d',amount);
 
-amount = Interest.setInterest(.10).setYears(10).setIsCompounding(false).setIncludePrincable(true).howMuchInterest(200);
+amount = Interest.setInterest(.10).setYears(10).setIsCompounding(false).setIncludePrincipal(true).howMuchInterest(200);
 console.log('e',amount);
 
 amount = Interest.interest(100,1,.1);
