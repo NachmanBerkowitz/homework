91b9074d06eda1d13b790506705073eaf623961f
NachmanBerkowitz <bernach4@gmail.com>
2018-07-10 17:10:44 -0400

    fixed return style of lamdas

--- 57/app.counterGetter.js
+++ 57/app.counterGetter.js
@@ -2,19 +2,19 @@ var app = app || {};
 app.counterGetter = (function (cg) {
     'use strict';
     let counterTracker = 0;
-    cg.getAmountOfCounters = () => {return counterTracker;};
+    cg.getAmountOfCounters = () => counterTracker;
     cg.getCounter = () => {
 
         counterTracker++;
 
         let count = 0;
         const counter =  {
-            getCount : () => { return count; },
+            getCount : () => count,
 
             increment : () => { count++; return counter; }
         };
         return counter;
-    }
+    };
     return cg;
 
 }(app.counterGetter || {}));
