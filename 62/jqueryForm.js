4b0e53d807cc712a460a0764e0529d5014d466ff
NachmanBerkowitz <bernach4@gmail.com>
2018-08-13 19:20:44 -0400

    62

---
+++ 62/jqueryForm.js
@@ -0,0 +1,16 @@
+/* global $ */
+const signUpForm= $('#signUpForm');
+const signUpTable= $('#signUpTable');
+const signUpButton = $('#signUpButton');
+
+signUpButton.click((event)=>{
+    event.preventDefault();
+    signUpTable.append(`<tr>
+    <td>${$('#name').val()}</td>
+    <td>${$('#email').val()}</td>
+    </tr>`)
+
+    signUpTable.show();
+
+    signUpForm[0].reset();
+});
