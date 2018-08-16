cfad1549d693c844e161cda6871a64f9b97288f6
NachmanBerkowitz <bernach4@gmail.com>
2018-08-16 19:53:05 -0400

    63.1

---
+++ 63/usingAjax.js
@@ -0,0 +1,40 @@
+/* global $ */
+(function () {
+    'use strict';
+    const dataViewer = $('#loadedDataViewer');
+    const loadButton = $('button');
+
+
+    const waiting = () => {
+        dataViewer.css({ 'font-size': '5em', 'transition': 'all 10s ease' })
+        dataViewer.html('hold on');
+    }
+
+    const finishedWaiting = ()=>{
+        dataViewer.css({ 'font-size': 'inherit', 'transition': 'none' })
+    }
+
+    const postToScreen = (data) => {
+
+
+        if (data.length === 0) {
+            data = 'Empty file found';
+        }
+        dataViewer.css('border', 'black solid 1px');
+        dataViewer.html(data);
+    };
+
+    $(loadButton).on('click', () => {
+        const userInput = $('#userInput').val();
+        if (userInput.length !== 0) {
+            waiting();
+            setTimeout(() => {
+                finishedWaiting();
+                $.get(userInput, postToScreen)
+                    .fail(postToScreen('Some error has accured. Try a diferent file'));
+            }, 3000)
+        }
+    });
+
+
+}());
