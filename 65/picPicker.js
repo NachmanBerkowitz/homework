882f9e8d4ca25a7f93c1e08129a06dc653aeb102
NachmanBerkowitz <bernach4@gmail.com>
2018-08-23 20:29:52 -0400

    65.1

---
+++ 65/picPicker.js
@@ -0,0 +1,69 @@
+/*global $*/
+(function () {
+    'use strict';
+
+    const pickerInput = $('#pickerInput');
+    const theButton = $('#theButton');
+    const image = $('#thePic');
+    const title = $('#title');
+    const previous = $('#previous');
+    const next = $('#next');
+
+    let previousSelection = '';
+    let pictures = [];
+    let picsSize = 0;
+    let peviousIndex = 0;
+
+    getPics('smiley face');
+
+    theButton.on('click', () => {
+        if (pickerInput.val() !== previousSelection) {
+            previousSelection = pickerInput.val();
+            getPics(pickerInput.val());
+        }
+    });
+
+    pickerInput.on('keyup', (event) => {
+        if (event.keyCode === 13) {
+            theButton.click();
+        }
+    });
+
+    function getPics(selection) {
+        $.getJSON(`https://api.flickr.com/services/feeds/photos_public.gne?tags=${selection}&format=json&jsoncallback=?`)
+            .done((data) => {
+                console.log(data);
+                parsePics(data.items);
+                showPic(0);
+            })
+            .fail((error) => {
+                console.log(error);
+            });
+    }
+
+    const parsePics = (pics) => {
+        picsSize = pics.length;
+        pictures = pics;
+    };
+
+    function showPic(picIndex) {
+        const src = pictures[picIndex].media.m;
+        const theTitle = pictures[picIndex].title;
+        image.attr('src', src);
+        title.text(theTitle);
+    }
+
+    next.on('click', () => {
+        const pic = (peviousIndex+1 < picsSize) ? peviousIndex+1 : 0;
+        showPic(pic);
+        peviousIndex = pic;
+    });
+
+    previous.on('click', () => {
+        const pic = (peviousIndex-1 >= 0) ? peviousIndex-1 : picsSize-1;
+        showPic(pic);
+        peviousIndex = pic;
+    });
+
+
+}());
