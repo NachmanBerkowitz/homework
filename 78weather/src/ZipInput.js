7b2a383c9ec3d3b10d0e2a0fef2abb4fce395f69
NachmanBerkowitz <bernach4@gmail.com>
2018-11-04 16:51:25 -0500

    78.2.4

--- 78weather/src/ZipInput.js
+++ 78weather/src/ZipInput.js
@@ -9,7 +9,7 @@ export default class ZipInput extends Component {
     const target = event.target;
     let value = target.type === 'checkbox' ? target.checked : target.value;
     console.log(value);
-    if(!value||!this.validateIsDigit(value[value.length-1])){
+    if(value && !this.validateIsDigit(value[value.length-1])){
       value = this.state.zipInput;
     }
     this.setState({
@@ -23,15 +23,15 @@ export default class ZipInput extends Component {
   }
 
   validateZip(num) {
-    return num.split('').every(char=>this.validateIsDigit(char));
+    return num.length === 5 && num.split('').every(char=>this.validateIsDigit(char));
   }
   
   handleSubmit = event => {
     const zip = this.state.zipInput;
     if (this.validateZip(zip)) {
       this.props.updateCurrentZip(zip);
+      event.preventDefault();
     }
-    event.preventDefault();
   };
 
   render() {
