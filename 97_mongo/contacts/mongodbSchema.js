e35641e5fca63f3ebbf63806ca9e61af0baec542
Nachman Berkowitz <38742354+NachmanBerkowitz@users.noreply.github.com>
2019-01-27 22:30:17 -0500

    database

---
+++ 97_mongo/contacts/mongodbSchema.js
@@ -0,0 +1,37 @@
+const database = {
+    database: 'contacts',
+    collections: [
+        {
+            contacts: [
+                {
+                    _id: ObjectId('5c4b4b095e597e5d9860ce89'),
+                    firstname: 'Nachman',
+                    lastname: 'Berkowitz',
+                    phone: '6464103346',
+                    email: 'bernach4@gmail.com',
+                },
+                {
+                    _id: ObjectId('5c4e63ef3286b594ecbd2f03'),
+                    firstname: 'mordy',
+                    lastname: 'feffer',
+                    phone: '4433456321',
+                    email: 'a@c.com',
+                },
+                {
+                    _id: ObjectId('5c4e64083286b594ecbd2f04'),
+                    firstname: 'dovy',
+                    lastname: 'mendelowitz',
+                    phone: '4567876543',
+                    email: '1@3.org',
+                },
+                {
+                    _id: ObjectId('5c4e65abe5dca29ebcb8c0c3'),
+                    firstname: 'dovid',
+                    lastname: 'lipshitz',
+                    phone: '9998887777',
+                    email: 'for@get.is',
+                },
+            ],
+        },
+    ],
+};
